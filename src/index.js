b7d3c2707a45dd0b23ea00f52c9a99156e050174
Nuno Monteiro <nfmmonteiro@gmail.com>
2015-09-19 20:23:44 +0100

    Adds interfaces to get country by alpha2 and alpha3 codes.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-var _ = require('lodash'),
-	countries = require('./countries.json').countries,
-	countriesModule = {};
+var countries = require('./countries.json').countries,
+	_ = require('lodash');
 
-countriesModule.getAllCountryNames = function() {
+function getAllCountryNames() {
 	var countryNames = countries.map(function(country) {
 		return country.name;
 	});
 	return countryNames;
+}
+
+function getByAlpha2Code(code) {
+	var country = _.find(countries, function(country) {
+		return (country.alpha2Code === code);
+	});
+	return country;
 };
 
-module.exports = countriesModule;
+function getByAlpha3Code(code) {
+	var country = _.find(countries, function(country) {
+		return (country.alpha3Code === code);
+	});
+	return country;
+};
+
+module.exports = {
+	getAllCountryNames: getAllCountryNames,
+	getByAlpha2Code: getByAlpha2Code,
+	getByAlpha3Code: getByAlpha3Code
+};
