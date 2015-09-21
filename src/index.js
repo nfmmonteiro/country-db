df7d5514b673e956da1e016531bb2e0ef1415e7e
Nuno Monteiro <nfmmonteiro@gmail.com>
2015-09-21 23:23:51 +0100

    chore(build): Adds JSCS code style linter
    Adds JSCS code style linter to enforce google javascript style guide

--- src/index.js
+++ src/index.js
@@ -2,32 +2,31 @@ import _ from 'lodash';
 import {countries} from './countries.json';
 
 function getAllCountryNames(region) {
-	var regionCountries, regionCountryNames;
-	regionCountries = _.filter(countries, function(country) {
-		return (!region || country.region === region);
-	});
-	regionCountryNames = regionCountries.map(function(country) {
-		return country.name;
-	});
-	return regionCountryNames;
+  var regionCountries = _.filter(countries, function(country) {
+    return (!region || country.region === region);
+  });
+  var regionCountryNames = regionCountries.map(function(country) {
+    return country.name;
+  });
+  return regionCountryNames;
 }
 
 function getByAlpha2Code(code) {
-	var country = _.find(countries, function(country) {
-		return (country.alpha2Code === code);
-	});
-	return country;
+  var country = _.find(countries, function(country) {
+    return (country.alpha2Code === code);
+  });
+  return country;
 };
 
 function getByAlpha3Code(code) {
-	var country = _.find(countries, function(country) {
-		return (country.alpha3Code === code);
-	});
-	return country;
+  var country = _.find(countries, function(country) {
+    return (country.alpha3Code === code);
+  });
+  return country;
 };
 
 module.exports = {
-	getAllCountryNames: getAllCountryNames,
-	getByAlpha2Code: getByAlpha2Code,
-	getByAlpha3Code: getByAlpha3Code
+  getAllCountryNames: getAllCountryNames,
+  getByAlpha2Code: getByAlpha2Code,
+  getByAlpha3Code: getByAlpha3Code
 };
